56334f38f14eed6d190d08894516b6af98fd06dd
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-06-28 19:50:58 +0530

    Add ProjectDirectoryNameManager.kt

---
+++ plugin/src/main/kotlin/io/github/heyysudarshan/gragle/handler/GragleConfigurationHandler.kt
@@ -0,0 +1,6 @@
+package io.github.heyysudarshan.gragle.handler
+
+object GragleConfigurationHandler {
+    var customBuildDirectory: Boolean = true
+    var assignGroupIdAndVersion: Boolean = true
+}
