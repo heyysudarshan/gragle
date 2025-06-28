4c38df83314f2a01b038b92510f6764a4443fc37
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-06-28 11:09:25 +0530

    Initial commit

---
+++ readme.md
@@ -0,0 +1,9 @@
+### About this repository
+
+Gradle is a build automation tool designed to simplify and streamline build configurations across all Kotlin projects,
+including Kotlin Multiplatform, Compose Multiplatform, Android, and more. It offers abstract APIs that make build setup
+easy and enjoyable for developers. By default, it handles tasks like build directory setup, group and ID assignment, and
+many other configurations automatically.Gradle is a build automation tool designed to simplify and streamline build
+configurations across all Kotlin projects, including Kotlin Multiplatform, Compose Multiplatform, Android, and more. It
+offers abstract APIs that make build setup easy and enjoyable for developers. By default, it handles tasks like build
+directory setup, group and ID assignment, and many other configurations automatically.
---
+++ settings.gradle.kts
@@ -0,0 +1,35 @@
+rootProject.name = "Gragle"
+
+@Suppress("UnstableApiUsage")
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
