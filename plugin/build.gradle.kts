dc76fd70ee81206f3f7b394b4f199c3ad7352fe4
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-06-28 11:47:53 +0530

    Configure Gragle plugin

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -18,5 +18,16 @@ kotlin.compilerOptions {
     jvmTarget = JvmTarget.JVM_11
 }
 
+val pluginName = libs.versions.gragle.name.get()
+gradlePlugin {
+    plugins {
+        create(pluginName) {
+            id = libs.versions.gragle.groupId.get()
+            version = libs.versions.gragle.version.get()
+            implementationClass = libs.versions.gragle.implementationClass.get()
+        }
+    }
+}
+
 // Custom build directory
 layout.buildDirectory.set(file("$rootDir/.build/plugin"))
