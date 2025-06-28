720de3f92fe46992e0fdd79885f2b2e18b2b6c33
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-06-28 11:25:33 +0530

    Set taget compatibility to Java 11

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,5 +1,21 @@
+import org.gradle.internal.jvm.inspection.JvmVendor
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.kotlin.compiler)
+}
+
+java {
+    toolchain {
+        version = JavaLanguageVersion.of(11)
+        vendor = JvmVendorSpec.ADOPTIUM
+    }
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+kotlin.compilerOptions {
+    jvmTarget = JvmTarget.JVM_11
 }
 
 // Custom build directory
