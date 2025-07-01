79bb84d276813f722c531a3b39f6a4b7e9013c7f
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-01 20:07:52 +0530

    Create layout.js

---
+++ docs/app/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+    "title": "Gragle - Documentation",
+}
+
+export default function RootLayout({content}) {
+    return (
+        <html lang="en">
+            <body>{content}</body>
+        </html>
+    );
+}
