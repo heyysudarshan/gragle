a1f8a48adc1a2a5d83de7d8c910e17143115ebd5
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-02 20:39:47 +0530

    Create PageInfo.js to store page information

---
+++ docs/app/PageInfo.js
@@ -0,0 +1,6 @@
+const pageInfo = {
+    "title": "Gragle - Build Configuration Automation Plugin",
+    "description": "Gragle is a build configuration automation plugin that simplifies and streamlines build configurations and project setup across all Kotlin-based Gradle projects, including Kotlin Multiplatform, Compose Multiplatform, Android, and more. It provides abstract APIs to make build setup easier and more enjoyable for developers working with Gradle projects."
+}
+
+export default pageInfo;
