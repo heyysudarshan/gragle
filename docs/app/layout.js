181d580f06f2bcabe24287dac55fa0fbd5c5df9b
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-02 20:37:01 +0530

    Re-create layout.js

--- docs/app/layout.js
+++ docs/app/layout.js
@@ -1,11 +1,11 @@
-export const metadata = {
-    "title": "Gragle - Documentation",
-}
-
-export default function RootLayout({content}) {
+export default function RootLayout() {
     return (
         <html lang="en">
-            <body>{content}</body>
+            <body className="content-body" id="content-body">
+                <div className="next-app" id="next-app">
+
+                </div>
+            </body>
         </html>
     );
 }
