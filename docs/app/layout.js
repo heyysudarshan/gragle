f86f194f4abfe7518ace87e26da91833031d1b4e
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-03 13:38:10 +0530

    Import all styles in layout.js

--- docs/app/layout.js
+++ docs/app/layout.js
@@ -1,13 +1,20 @@
 import pageInfo from "@/app/PageInfo";
+import Header from "@/components/header/Header";
+
+import "styles/App.css";
+import "styles/Type.css";
+import "styles/Color.css";
+import "styles/Spacing.css";
 
 export const metadata = pageInfo;
 
-export default function RootLayout() {
+export default function RootLayout({children}) {
     return (
         <html lang="en">
             <body className="content-body" id="content-body">
                 <div className="next-app" id="next-app">
-
+                    <Header />
+                    <main className="main-content" id="main-content">{children}</main>
                 </div>
             </body>
         </html>
