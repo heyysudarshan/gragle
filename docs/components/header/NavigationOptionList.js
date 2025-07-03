9d8212bf50eba455b0c15d7353c9ad9fec3ce806
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-03 11:19:42 +0530

    Create NavigationOptionList.js to store navigation options

---
+++ docs/components/header/NavigationOptionList.js
@@ -0,0 +1,34 @@
+const navigationOptionList = [
+    {
+        id: "home",
+        label: "Home",
+        endpoint: "/"
+    },
+    {
+        id: "learn-gragle",
+        label: "Learn Gragle",
+        endpoint: "/learn-gragle"
+    },
+    {
+        id: "blogs",
+        label: "Blogs",
+        endpoint: "/blogs"
+    },
+    {
+        id: "contribute",
+        label: "Contribute",
+        endpoint: "/contribute"
+    },
+    {
+        id: "design-thinking",
+        label: "Design Thinking",
+        endpoint: "/design-thinking"
+    },
+    {
+        id: "gragle-wizard",
+        label: "Gragle Wizard",
+        endpoint: "/gragle-wizard"
+    }
+]
+
+export default navigationOptionList;
