5d709d58c5e0f9714647bac6a86354e96c021e38
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-03 13:31:44 +0530

    Create NavigationOption component for navigation elements

---
+++ docs/components/header/NavigationOption.js
@@ -0,0 +1,37 @@
+"use client"
+
+import Link from "next/link";
+import React from "react";
+import { usePathname } from "next/navigation";
+import Spacer from "@/components/Spacer";
+
+const NavigationOption = ({id, label, route}) => {
+    const isSelected = usePathname() === route;
+    const navigationOptionId = `navigation-option-${id}`;
+
+    let labelStyle = "navigation-option-link ";
+    if (isSelected) {
+        labelStyle += "primary-color-text body-large semibold"
+    } else {
+        labelStyle += "secondary-font-color body-large regular"
+    }
+
+    let indicatorStyle = "indicator ";
+    if (isSelected) {
+        indicatorStyle += "primary-color"
+    } else {
+        indicatorStyle += ""
+    }
+
+    return (
+        <li className="navigation-option" id={navigationOptionId} key={id}>
+            <Link href={route} className={labelStyle}>
+                {label}
+                <Spacer className="_8px"></Spacer>
+                <div className={indicatorStyle}>.</div>
+            </Link>
+        </li>
+    );
+};
+
+export default NavigationOption;
