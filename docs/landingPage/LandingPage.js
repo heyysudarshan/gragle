9df88317d82907b31e3d32b3aeb1dd8456dee775
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-05 15:50:00 +0530

    Create LandingPage.js

---
+++ docs/landingPage/LandingPage.js
@@ -0,0 +1,12 @@
+import "./LandingPage.css";
+import HeroSection from "@/landingPage/heroSection/HeroSection";
+
+const LandingPage = () => {
+    return (
+        <article className="landing-page" id="landing-page">
+            <HeroSection/>
+        </article>
+    );
+};
+
+export default LandingPage;
