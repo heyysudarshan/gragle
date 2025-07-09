1f8684533e95a3a2b29ef8491e557e6f59b774f1
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-09 19:55:44 +0530

    Define plugin meta-data in build.gradle.kts of plugin module

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -2,3 +2,16 @@ plugins {
     alias(libs.plugins.kotlin.compiler)
     alias(libs.plugins.gradle.plugin.development)
 }
+
+group = libs.versions.plugin.groupId.get()
+version = libs.versions.plugin.version.get()
+
+gradlePlugin {
+    plugins {
+        create("Gragle") {
+            id = libs.versions.plugin.groupId.get()
+            version = libs.versions.plugin.version.get()
+            implementationClass = libs.versions.plugin.implementationClass.get()
+        }
+    }
+}
