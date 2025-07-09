94719dd1220ab48dd5404a85383e166c52fbe1ba
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-09 21:39:02 +0530

    Create internal function configureGroupIdAndVersion() to configure groupId and version for projects

---
+++ plugin/src/main/kotlin/io/github/heyysudarshan/gragle/config/GroupIdAndVersionConfig.kt
@@ -0,0 +1,37 @@
+package io.github.heyysudarshan.gragle.config
+
+import io.github.heyysudarshan.gragle.util.ProjectDirectoryNameManager
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
+
+internal fun configureGroupIdAndVersion(project: Project) {
+    val libs = project.extensions.getByType(VersionCatalogsExtension::class.java)
+        .named("libs")
+
+    val formattedProjectDirectoryName = ProjectDirectoryNameManager.getFormattedProjectDirectoryName(
+        projectDirectoryName = project.name
+    )
+
+    val groupIdIdentifier = "$formattedProjectDirectoryName-groupId"
+    val versionIdentifier = "$formattedProjectDirectoryName-version"
+
+    val groupId = libs.findVersion(groupIdIdentifier).orElseThrow {
+        IllegalArgumentException(
+            """
+                To let Gragle set the groupId for the ${project.name} module automatically, add a version for `$groupIdIdentifier` in your version catalog.
+                If you don’t want Gragle to do this, just set `assignGroupIdAndVersion = false` in the gragle {} block of your root build.gradle.kts file.
+            """.trimIndent()
+        )
+    }
+    val version = libs.findVersion(versionIdentifier).orElseThrow {
+        IllegalArgumentException(
+            """
+                To let Gragle set the groupId for the ${project.name} module automatically, add a version for `$versionIdentifier` in your version catalog.
+                If you don’t want Gragle to do this, just set `assignGroupIdAndVersion = false` in the gragle {} block of your root build.gradle.kts file.
+            """.trimIndent()
+        )
+    }
+
+    project.group = groupId
+    project.version = version
+}
