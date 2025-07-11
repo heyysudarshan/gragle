51e733c4b2b2212ee1ba417416ac9fcaaa385590
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-11 12:30:27 +0530

    Create ProjectWizardPage component with basic structure

---
+++ docs/project-wizard-page/ProjectWizardPage.js
@@ -0,0 +1,9 @@
+const ProjectWizardPage = () => {
+    return (
+        <article className="project-wizard-page" id="project-wizard-page">
+
+        </article>
+    );
+};
+
+export default ProjectWizardPage;
