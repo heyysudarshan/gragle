e1bf3a561253749e86c24c478b0b5fcf1c9873d6
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-11 16:20:48 +0530

    Add documentation for `gragle` global settings configuration extension

--- plugin/src/main/kotlin/io/github/heyysudarshan/config/GragleConfiguration.kt
+++ plugin/src/main/kotlin/io/github/heyysudarshan/config/GragleConfiguration.kt
@@ -3,6 +3,14 @@ package io.github.heyysudarshan.config
 import io.github.heyysudarshan.handler.GlobalConfigurationHandler
 import org.gradle.api.Project
 
+/**
+ * Extension function to configure Gragle's global settings.
+ *
+ * This function provides access to the [GlobalConfigurationHandler], allowing you to enable or disable
+ * various Gragle features such as custom build directory locations, automatic group ID assignment, and more.
+ *
+ * Note: Changes made using this configuration will apply globally and affect all modules in the project.
+ */
 fun Project.gragle(globalConfigurationHandlerScope: GlobalConfigurationHandler.() -> Unit) {
     globalConfigurationHandlerScope(GlobalConfigurationHandler)
 }
