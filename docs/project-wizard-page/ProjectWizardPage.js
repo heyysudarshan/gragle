22dd7326366d3f3fbcde0f74ea6a2c29f4eb4a01
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-11 12:54:54 +0530

    Add ProjectWizardTitleAndMessage component to ProjectWizardPage

--- docs/project-wizard-page/ProjectWizardPage.js
+++ docs/project-wizard-page/ProjectWizardPage.js
@@ -1,7 +1,21 @@
+import "./ProjectWizardPage.css";
+
+const title = "Gragle Project Wizard"
+const message = "This project wizard generates a Gradle project with the Gradle plugin pre-applied. You can set up Android, Kotlin Multiplatform, and other supported modules using Gradle APIs."
+
+const ProjectWizardTitleAndMessage = () => {
+    return (
+        <section className="project-wizard-title-and-message" id="project-wizard-title-and-message">
+            <h1 className="project-wizard-title title-medium semi-bold" id="project-wizard-title">{title}</h1>
+            <p className="project-wizard-message body-medium regular" id="project-wizard-message">{message}</p>
+        </section>
+    );
+};
+
 const ProjectWizardPage = () => {
     return (
         <article className="project-wizard-page" id="project-wizard-page">
-
+            <ProjectWizardTitleAndMessage></ProjectWizardTitleAndMessage>
         </article>
     );
 };
