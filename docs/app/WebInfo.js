fd199aeffbb71bf1f0f9b054424188e4d6f4a665
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-11 11:39:53 +0530

    Add WebInfo.js to define site metadata

---
+++ docs/app/WebInfo.js
@@ -0,0 +1,6 @@
+const WebInfo = {
+    title: "Gragle: Build Configuration Automation for Kotlin",
+    description: "A Gradle plugin designed to automate and simplify build configurations for Kotlin-based projects.",
+}
+
+export default WebInfo;
