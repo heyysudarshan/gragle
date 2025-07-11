161660d626d2071e476707d7128774dfc96e3587
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-11 11:40:00 +0530

    Create layout.js to define the main application structure

---
+++ docs/app/layout.js
@@ -0,0 +1,15 @@
+const NextApp = () => {
+    return (
+        <html lang="en">
+            <head>
+                <meta name="description" content={WebInfo.description} />
+                <title>{WebInfo.title}</title>
+            </head>
+            <body>
+
+            </body>
+        </html>
+    );
+};
+
+export default NextApp;
