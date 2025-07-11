86fd8f700d2099469c2a08bbda9604c5660bb065
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-11 18:25:10 +0530

    Update `gragle` configuration docs to include `createModule` API usage example

--- plugin/src/main/kotlin/io/github/heyysudarshan/config/GragleConfiguration.kt
+++ plugin/src/main/kotlin/io/github/heyysudarshan/config/GragleConfiguration.kt
@@ -9,6 +9,9 @@ import org.gradle.api.Project
  * This function provides access to the [GlobalConfigurationHandler], allowing you to enable or disable
  * various Gragle features such as custom build directory locations, automatic group ID assignment, and more.
  *
+ * By accessing the scope of [GlobalConfigurationHandler] in this extension function's lambda block, you can also
+ * use Gragle APIs like `createModule` to automatically add new modules to your existing project declaratively.
+ *
  * Note: Changes made using this configuration will apply globally and affect all modules in the project.
  */
 fun Project.gragle(globalConfigurationHandlerScope: GlobalConfigurationHandler.() -> Unit) {
