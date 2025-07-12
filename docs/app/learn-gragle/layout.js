65e2f9f3aceb02db4d945cf75e9c2458c82a8294
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-12 18:11:21 +0530

    Remove redundant `id` attributes in `LearnGragle` layout component for cleaner markup.

--- docs/app/learn-gragle/layout.js
+++ docs/app/learn-gragle/layout.js
@@ -2,14 +2,10 @@ import "./Style.css";
 
 const LearnGragle = ({children}) => {
     return (
-        <article className="learn-gragle" id="learn-gragle">
-            <nav className="learn-gragle-primary-navigation" id="learn-gragle-primary-navigation">
-
-            </nav>
-            <div className="article-content" id="article-content">{children}</div>
-            <aside className="learn-gragle-secondary-navigation" id="learn-gragle-secondary-navigation">
-
-            </aside>
+        <article className="learn-gragle">
+            <nav className="learn-gragle-primary-navigation"></nav>
+            <div className="article-content">{children}</div>
+            <aside className="learn-gragle-secondary-navigation"></aside>
         </article>
     );
 };
