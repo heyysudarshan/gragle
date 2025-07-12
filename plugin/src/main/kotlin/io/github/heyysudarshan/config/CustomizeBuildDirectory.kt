a67f1f29693f8449f297086b1e8d3ff093fed36d
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-12 12:22:38 +0530

    Refactor `customizeBuildDirectory` function: simplify line formatting and remove redundant newline.

--- plugin/src/main/kotlin/io/github/heyysudarshan/config/CustomizeBuildDirectory.kt
+++ plugin/src/main/kotlin/io/github/heyysudarshan/config/CustomizeBuildDirectory.kt
@@ -4,10 +4,9 @@ import io.github.heyysudarshan.util.ProjectDirectoryNameManager
 import org.gradle.api.Project
 
 internal fun customizeBuildDirectory(project: Project) {
-    val projectDirectoryFormattedName = ProjectDirectoryNameManager.getFormattedProjectDirectoryName(
-        projectDirectoryName = project.name
-    )
-    val buildDirectoryName = "${project.rootProject}/.build/$projectDirectoryFormattedName"
+    val projectDirectoryFormattedName = ProjectDirectoryNameManager
+        .getFormattedProjectDirectoryName(projectDirectoryName = project.name)
 
+    val buildDirectoryName = "${project.rootProject}/.build/$projectDirectoryFormattedName"
     project.layout.buildDirectory.set(project.file(buildDirectoryName))
 }
