dd7c7c9accfcdae93c2b43093bdfe711bed4019d
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-12 11:49:19 +0530

    Refactor `createModule` API: simplify signature and remove unused `ModuleType`.

--- plugin/src/main/kotlin/io/github/heyysudarshan/extension/CreateModule.kt
+++ plugin/src/main/kotlin/io/github/heyysudarshan/extension/CreateModule.kt
@@ -3,16 +3,7 @@ package io.github.heyysudarshan.extension
 import io.github.heyysudarshan.handler.GlobalConfigurationHandler
 import io.github.heyysudarshan.handler.GlobalModuleConfigurationHandler
 
-enum class ModuleType {
-    Android,
-    Desktop,
-    KotlinMultiplatformLibrary
-}
-
-data class Module(
-    val type: ModuleType,
-    val moduleName: String
-)
+object CreateModulesScope
 
 /**
  * Extension function to create a new module inside your existing Gradle project using Gragle.
@@ -21,27 +12,15 @@ data class Module(
  * anywhere, but to improve readability and expect predictable behavior, it's scoped under the lambda body of the
  * [createModule] function.
  *
- * When you call this function, it takes a lambda function as its parameter, which should return [Module] as its return
- * value. You can pass any of the following functions as its argument to create a new module:
+ * When you call this function, it takes a lambda function as its argument, which returns a String as its return value.
+ * You can pass any of the following functions as its argument to create a new module:
  *
  * 1. [kotlinMultiplatformLibrary]: This function creates a new Kotlin multiplatform library module inside your project.
  * You can pass this function as an argument for the [createModule] function.
  */
 fun GlobalConfigurationHandler.createModule(
-    module: KotlinMultiplatformConfigScope.() -> Module
+    module: CreateModulesScope.() -> String
 ) {
-    val currentModule = module(KotlinMultiplatformConfigScope)
-    when(currentModule.type) {
-        ModuleType.KotlinMultiplatformLibrary -> {
-
-        }
-        else -> {
-
-        }
-    }
-}
-
-
-private fun handleKotlinMultiplatformModuleCreation(moduleName: String) {
-    GlobalModuleConfigurationHandler.configure(moduleName = moduleName)
+    val currentModuleName = module(CreateModulesScope)
+    GlobalModuleConfigurationHandler.configure(moduleName = currentModuleName)
 }
