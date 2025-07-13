2edad65d554a4acf0bb8e5adc7e846442a4f9ba8
Sudarshan Mhasrup <sudarshanmhasrup@gmail.com>
2025-07-13 21:17:27 +0530

    Add arrow icon and spacing to `NextArticleNavigator` for improved navigation layout

--- docs/components/NextArticleNavigator.js
+++ docs/components/NextArticleNavigator.js
@@ -1,6 +1,7 @@
-import "./Components.css";
 import Spacer from "@/components/Spacer";
 import Link from "next/link";
+
+import "./Components.css";
 
 const NextArticleNavigator = ({label, route}) => {
     return (
@@ -10,6 +11,8 @@ const NextArticleNavigator = ({label, route}) => {
                 <Spacer className="spacer-8px-height"></Spacer>
                 <span className="next-article-navigator-label body-medium semi-bold primary-font-color">{label}</span>
             </span>
+            <Spacer className="spacer-20px-width"></Spacer>
+            <img className="next-article-navigator-arrow" src="/assets/arrow-right.svg" alt="Next Article Arrow"/>
         </Link>
     );
 };
